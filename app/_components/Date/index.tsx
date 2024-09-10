e88e96c84d052ef3854ca192818b209f9c64e500
monaka496 <monaka496@outlook.com>
2024-09-10 23:45:41 +0900

    6章まで完了

--- app/_components/Date/index.tsx
+++ app/_components/Date/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import style from "./index.module.css";
+import { formatDate } from "@/app/_libs/utils";
 
 type Props = {
   date: string;
@@ -9,7 +10,7 @@ export default function Date({ date }: Props) {
   return (
     <span className={style.date}>
       <Image src="/clock.svg" alt="" width={16} height={16} loading="eager" />
-      {date}
+      {formatDate(date)}
     </span>
   );
 }
