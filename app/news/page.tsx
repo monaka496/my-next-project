ae862951f6b640aa953e8ca04bcb703903954702
monaka496 <monaka496@outlook.com>
2024-09-19 23:10:42 +0900

    7章まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,19 @@
 import { getNewsList } from "../_libs/microcms";
 import NewsList from "../_components/NewsList";
+import Pagenation from "../_components/Pagenation";
+import { NEWS_LIST_LIMIT } from "../_constants";
+import SearchField from "../_components/SearchField";
 
 export default async function Page() {
-  const { contents: news } = await getNewsList();
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+  });
 
-  return <NewsList news={news} />;
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+      <Pagenation totalCount={totalCount} />
+    </>
+  );
 }
