b1c3e9827065aa5716faf5a5ff7aa99d128b6909
monaka496 <monaka496@outlook.com>
2024-09-25 18:56:56 +0900

    form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,42 @@
-import ContactForm from "@/app/_components/ContactForm";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <div className={styles.container}>
+    <div>
       <p className={styles.text}>
         ご質問、ご相談は下記フォームよりお問い合わせください。
         <br />
         内容確認後、担当者より通常3営業日以内にご連絡いたします。
       </p>
-      <ContactForm />
+      <form
+        className={styles.form}
+        action="https://ssgform.com/s/i767b5V39NXh"
+        method="post"
+      >
+        <input
+          className={styles.input}
+          type="text"
+          name="お名前"
+          placeholder="お名前"
+          required
+        />
+        <input
+          className={styles.input}
+          type="email"
+          name="メールアドレス"
+          placeholder="メールアドレス"
+          required
+        />
+        <textarea
+          className={styles.textarea}
+          name="お問い合わせ内容"
+          placeholder="お問い合わせ内容"
+          required
+        ></textarea>
+        <button className={styles.button} type="submit">
+          送信する
+        </button>
+      </form>
     </div>
   );
 }
