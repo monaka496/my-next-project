f8b905d6c846373ea567ad5ae01fb76054f7db0f
monaka496 <monaka496@outlook.com>
2024-11-02 23:32:12 +0900

    ad

--- app/_components/SideMenu/index.tsx
+++ app/_components/SideMenu/index.tsx
@@ -3,6 +3,7 @@ import type { Category, Tag } from "@/app/_libs/microcms"; // Category,Tag型を
 import SearchField from "../SearchField";
 import styles from "./index.module.css";
 import Link from "next/link";
+import Script from "next/script";
 
 type Props = {
   categories: Category[]; // カテゴリの配列をCategory型に変更
@@ -12,6 +13,21 @@ type Props = {
 export default function SideMenu({ categories, tags }: Props) {
   return (
     <>
+      {/* AdSenseの広告タグ */}
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-6783574511450629"
+        data-ad-slot="1648670387"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+
+      {/* AdSenseのスクリプトを読み込む */}
+      <Script id="adsbygoogle-init" strategy="afterInteractive">
+        {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+      </Script>
+
       <SearchField />
       <h3 className={styles.sidetitle}>カテゴリ</h3>
       <ul>
