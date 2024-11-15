d312417bca44768cedb51c863d945c573e9383d7
monaka496 <monaka496@outlook.com>
2024-11-16 08:01:22 +0900

    menu

--- app/_components/SideMenu/index.tsx
+++ app/_components/SideMenu/index.tsx
@@ -1,33 +1,17 @@
-// app_components/SideMenu/index.tsx
-import type { Category, Tag } from "@/app/_libs/microcms"; // Category,Tag型をインポート
+import type { Category, Tag } from "@/app/_libs/microcms";
 import SearchField from "../SearchField";
 import styles from "./index.module.css";
 import Link from "next/link";
 import Script from "next/script";
 
 type Props = {
-  categories: Category[]; // カテゴリの配列をCategory型に変更
-  tags: Tag[]; // タグの配列をTag型に変更
+  categories: Category[];
+  tags: Tag[];
 };
 
 export default function SideMenu({ categories, tags }: Props) {
   return (
     <>
-      {/* AdSenseの広告タグ */}
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-6783574511450629"
-        data-ad-slot="1648670387"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-
-      {/* AdSenseのスクリプトを読み込む */}
-      <Script id="adsbygoogle-init" strategy="afterInteractive">
-        {`(adsbygoogle = window.adsbygoogle || []).push({});`}
-      </Script>
-
       <SearchField />
       <h3 className={styles.sidetitle}>カテゴリ</h3>
       <ul>
@@ -58,6 +42,20 @@ export default function SideMenu({ categories, tags }: Props) {
             </li>
           ))}
       </ul>
+      {/* AdSenseの広告タグ */}
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-6783574511450629"
+        data-ad-slot="1648670387"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+
+      {/* AdSenseのスクリプトを読み込む */}
+      <Script id="adsbygoogle-init" strategy="afterInteractive">
+        {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+      </Script>
     </>
   );
 }
