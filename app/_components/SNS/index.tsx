1e5d49f7864b064b6a90aaa790e626d41a7e54cc
monaka496 <monaka496@outlook.com>
2025-10-18 16:12:28 +0900

    SNS

--- app/_components/SNS/index.tsx
+++ app/_components/SNS/index.tsx
@@ -6,22 +6,27 @@ type Props = {
   title: string;
 };
 
-export const Share: React.FC<Props> = (props) => {
-  const { id, title } = props;
+export const Share: React.FC<Props> = ({ id, title }) => {
+  if (!id || !title) return null;
 
-  if (!id || !title) {
-    return null;
-  }
+  const url = `https://monaka496.com/blog/${id}/`;
 
   const twitterLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
     title
-  )}&url=https://monaka496.com/blog/${id}/`;
-  const facebookLink = `https://www.facebook.com/sharer.php?u=https://monaka496.com/blog/${id}/`;
-  const hatenaLink = `https://b.hatena.ne.jp/entry/https://monaka496.com/blog/${id}/`;
+  )}&url=${url}`;
+  const facebookLink = `https://www.facebook.com/sharer.php?u=${url}`;
+  const hatenaLink = `https://b.hatena.ne.jp/entry/${url}`;
+  const mastodonLink = `https://toot.kytta.dev/share?text=${encodeURIComponent(
+    `${title}\n${url}`
+  )}`;
+  const blueskyLink = `https://bsky.app/intent/compose?text=${encodeURIComponent(
+    `${title} ${url}`
+  )}`;
+  const noteLink = `https://note.mu/intent/post?url=${encodeURIComponent(url)}`;
 
   return (
     <div className={styles.share}>
-      <h3 className={styles.sharetitle}>記事をシェアする</h3>
+      <h3 className={styles.sharetitle}>＼　この記事をシェアする　／</h3>
       <ul className={styles.shareul}>
         <li>
           <a
@@ -33,11 +38,10 @@ export const Share: React.FC<Props> = (props) => {
             <Image
               src="/icon_x.svg"
               alt="Xでシェアする"
+              width={22}
+              height={22}
               className={styles.icon}
-              width={16}
-              height={16}
             />
-            ポストする
           </a>
         </li>
         <li>
@@ -50,11 +54,10 @@ export const Share: React.FC<Props> = (props) => {
             <Image
               src="/icon_facebook.svg"
               alt="Facebookでシェアする"
+              width={22}
+              height={22}
               className={styles.icon}
-              width={16}
-              height={16}
             />
-            シェアする
           </a>
         </li>
         <li>
@@ -67,11 +70,58 @@ export const Share: React.FC<Props> = (props) => {
             <Image
               src="/icon_hatena.svg"
               alt="はてなブックマークする"
+              width={22}
+              height={22}
               className={styles.icon}
-              width={16}
-              height={16}
             />
-            ブックマークする
+          </a>
+        </li>
+        <li>
+          <a
+            href={mastodonLink}
+            className={`${styles.sharebutton} ${styles.sharemstdn}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/icon_mastodon.svg"
+              alt="Mastodonでシェアする"
+              width={22}
+              height={22}
+              className={styles.icon}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href={blueskyLink}
+            className={`${styles.sharebutton} ${styles.sharebsky}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/icon_bluesky.svg"
+              alt="Blueskyでシェアする"
+              width={22}
+              height={22}
+              className={styles.icon}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href={noteLink}
+            className={`${styles.sharebutton} ${styles.sharenote}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/icon_note.svg"
+              alt="noteでシェアする"
+              width={22}
+              height={22}
+              className={styles.icon}
+            />
           </a>
         </li>
       </ul>
